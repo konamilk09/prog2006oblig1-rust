dba6129e14bd7cd12894d5ca3459a55245e624f4
Guangyi Zhu <coconut2927@gmail.com>
2023-04-25 10:33:09 +0200

    add more

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct User {
     active: bool,
     username: String,
@@ -5,14 +6,25 @@ struct User {
     sign_in_count: u64,
 }
 
+impl User {
+    fn count(&self) -> bool {
+        self.sign_in_count > 0
+    }
+}
+
 fn main() {
-    //struct
+    // struct
     let user1 = User {
         active: true,
         username: String::from("someusername123"),
         email: String::from("someone@example.com"),
         sign_in_count: 1,
     };
+
+    // method
+    if user1.count() {
+        println!("The user's sign in count is {}", user1.sign_in_count);
+    }
     
     // mutability
     let mut x = 5;
@@ -34,9 +46,21 @@ fn main() {
     let s = String::from("hello");  // s comes into scope
     takes_ownership(s);    // s's value moves into the function...
     // ... s is no longer valid here
+
+    // refactoring tuples
+    let rect1 = (30, 50);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(rect1)
+    );
+
 }
 
 fn takes_ownership(some_string: String) { // some_string comes into scope
     println!("{}", some_string);
 } // Here, some_string goes out of scope and `drop` is called. The backing
   // memory is freed.
+
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
